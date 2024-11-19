7871e697ccc333b71a884e959041e2bb586be02e
Leslie <Kooliesk@gmail.com>
2024-11-19 15:01:24 +0200

    Configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://bellville-travel-agency.netlify.app/"
 
     resource "*",
       headers: :any,
