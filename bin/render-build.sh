3026e3d443776c18e19c9a208a8e9b8d55d02d10
Leslie <Kooliesk@gmail.com>
2024-11-19 13:09:02 +0200

    Configure Puma

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
 
-# If you're using a Free instance type, you need to
-# perform database migrations in the build command.
-# Uncomment the following line:
-
-# bundle exec rails db:migrate
+# Migrate the database
+bundle exec rails db:migrate
