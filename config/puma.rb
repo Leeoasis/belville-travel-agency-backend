f617f6cefb9d8d22748e6acdf7edb1a590772793
Leslie <Kooliesk@gmail.com>
2024-11-26 11:37:11 +0200

    Final changes

--- config/puma.rb
+++ config/puma.rb
@@ -11,15 +11,15 @@ port ENV.fetch("PORT", 3001)
 environment ENV.fetch("RAILS_ENV", "production")
 
 # Number of Puma workers (default: 2)
-# workers ENV.fetch("WEB_CONCURRENCY", 2)
+workers ENV.fetch("WEB_CONCURRENCY", 2)
 
 # Preload the application before forking workers for performance
-# preload_app!
+preload_app!
 
 # Re-establish connections for ActiveRecord on worker boot
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
 
 # Allow Puma to be restarted by `bin/rails restart`
 plugin :tmp_restart
